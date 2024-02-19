1656dd5440cdf7e13301ef2d971d1b39604f079b
Lin Zhang <ljzhang@yahooinc.com>
2024-02-19 15:13:17 -0800

    draft

---
+++ myprojectdir/build.sh
@@ -0,0 +1,19 @@
+#
+
+# Create a virtual environment and activate it
+cd ~/myprojectdir
+python3 -m venv myprojectenv
+source myprojectenv/bin/activate
+
+# Install Django and other project dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+python manage.py makemigrations
+python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD=password python manage.py createsuperuser --username=admin --email=admin@digitalocean.com --noinput
+
+python manage.py runserver
---
+++ myprojectdir/deployment/setup_host.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# sh setup_host.sh
+sudo apt update
+sudo apt install python3-venv python3-dev libpq-dev postgresql postgresql-contrib nginx curl
